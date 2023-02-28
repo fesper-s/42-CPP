0017d0025c06147aa52dee33cfa20598e4d34c60
fesper-s <fafaeesfabricio@gmail.com>
2023-02-28 02:49:02 -0300

    done ex01

--- Module-03/ex01/ScavTrap.hpp
+++ Module-03/ex01/ScavTrap.hpp
@@ -6,18 +6,28 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 15:16:45 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/02/27 16:26:29 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/02/28 01:15:36 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_H
 # define SCAVTRAP_H
 
+#include "ClapTrap.hpp"
+
 class ScavTrap : public ClapTrap {
 	private:
-		
+		std::string _name;
 	public:
-		
+		ScavTrap(void);
+		ScavTrap(std::string name);
+		ScavTrap(ScavTrap const &scavTrap);
+		~ScavTrap(void);
+
+		ScavTrap &operator=(ScavTrap const &scavTrap);
+	
+		void attack(const std::string& target);
+		void guardGate();
 };
 
 #endif
