b02408c659311cbfe490141378b0da2755014081
fesper-s <fafaeesfabricio@gmail.com>
2023-04-20 08:08:38 -0300

    adding CPP05

---
+++ Module-05/Bureaucrat.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 08:04:07 by fesper-s          #+#    #+#             */
+/*   Updated: 2023/04/20 08:07:54 by fesper-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucra {
+	private:
+		const std::string _name;
+		int _grade;
+};
+
+#endif
