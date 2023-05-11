5642d19c660099f10a927381c66d5bdcb587b887
fesper-s <fafaeesfabricio@gmail.com>
2023-05-11 11:22:38 -0300

    adding checkPseudoLiterals

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -13,11 +13,28 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs) {
 	return *this;
 }
 
+static int checkPseudoLiterals(const std::string &literal) {
+	if (literal == "nan" || literal == "-inf" || literal == "+inf" || literal == "nanf" || literal == "-inff" || literal == "+inff") {
+		std::cout << "char: impossible" << std::endl;
+		std::cout << "int: impossible" << std::endl;
+		if (literal == "nan" || literal == "-inf" || literal == "+inf") {
+			std::cout << "float: " << literal << "f" << std::endl;
+			std::cout << "double: " << literal << std::endl;
+		}
+		else if (literal == "nanf" || literal == "-inff" || literal == "+inff") {
+			std::cout << "float: " << literal << std::endl;
+			std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
+		}
+		return 1;
+	}
+	return 0;
+}
+
 void ScalarConverter::convert(const std::string &literal) {
 	std::string typeChar = "";
 	int typeInt = 0;
-	float typeFloat = 0.0f;
-	double typeDouble = 0.0;
+	//float typeFloat = 0.0f;
+	//double typeDouble = 0.0;
 
 	if (literal.length() == 1 && isprint(literal[0]) && !isdigit(literal[0])) {
 		typeChar = literal[0];
@@ -27,4 +44,9 @@ void ScalarConverter::convert(const std::string &literal) {
 		std::cout << "double: " << static_cast<double>(typeChar[0]) << ".0" << std::endl;
 		return;
 	}
+
+	if (checkPseudoLiterals(literal))
+		return;
+	
+	typeInt = stoi(literal);
 }
--- Module-06/ex00/main.cpp
+++ Module-06/ex00/main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char **argv) {
 		std::cerr << "Error: wrong number of arguments" << std::endl;
 		return 1;
 	}
-	ScalarConverter::convert(argv[1]);
+	try {
+		ScalarConverter::convert(argv[1]);
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
