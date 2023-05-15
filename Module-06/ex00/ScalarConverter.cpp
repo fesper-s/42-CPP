c2733d8ca774172afeb0ac3104b0841cfae0ffbf
fesper-s <fesper-s@student.42.rio>
2023-05-15 10:46:38 -0300

    06-ex00 is done

--- Module-06/ex00/ScalarConverter.cpp
+++ Module-06/ex00/ScalarConverter.cpp
@@ -13,28 +13,21 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &rhs) {
 	return *this;
 }
 
-static int checkPseudoLiterals(const std::string &literal) {
-	if (literal == "nan" || literal == "-inf" || literal == "+inf" || literal == "nanf" || literal == "-inff" || literal == "+inff") {
-		std::cout << "char: impossible" << std::endl;
-		std::cout << "int: impossible" << std::endl;
-		if (literal == "nan" || literal == "-inf" || literal == "+inf") {
-			std::cout << "float: " << literal << "f" << std::endl;
-			std::cout << "double: " << literal << std::endl;
-		}
-		else if (literal == "nanf" || literal == "-inff" || literal == "+inff") {
-			std::cout << "float: " << literal << std::endl;
-			std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
-		}
-		return 1;
+static bool checkPseudoLiterals(const std::string &literal) {
+	std::string pseudoLiteral[6] = {"-inf", "+inf", "nan", "-inff", "+inff", "nanf"};
+
+	for (int i = 0; i < 6; i++) {
+		if (literal == pseudoLiteral[i])
+			return true;
 	}
-	return 0;
+	return false;
 }
 
 void ScalarConverter::convert(const std::string &literal) {
 	std::string typeChar = "";
 	int typeInt = 0;
-	//float typeFloat = 0.0f;
-	//double typeDouble = 0.0;
+	float typeFloat = 0.0f;
+	double typeDouble = 0.0;
 
 	if (literal.length() == 1 && isprint(literal[0]) && !isdigit(literal[0])) {
 		typeChar = literal[0];
@@ -45,8 +38,48 @@ void ScalarConverter::convert(const std::string &literal) {
 		return;
 	}
 
+	typeInt = atoi(literal.c_str());
+
+	if (literal[literal.length() - 1] == 'f') {
+		typeFloat = stof(literal);
+		typeDouble = static_cast<double>(typeFloat);
+	}
+	else {
+		typeDouble = stof(literal);
+		typeFloat = static_cast<float>(typeDouble);
+	}
+
 	if (checkPseudoLiterals(literal))
+		typeChar = "impossible";
+
+	if (typeChar.empty() && !isprint(typeInt))
+    	typeChar = "Non displayable";
+
+	if (typeChar.empty() && isprint(typeInt)) {
+		typeChar = "'";
+		typeChar += static_cast<char>(typeInt);
+		typeChar += "'";
+	}
+
+	std::cout << "char: " << typeChar << std::endl;
+
+	if (typeChar == "impossible")
+		std::cout << "int: impossible" << std::endl;
+	else
+		std::cout << "int: " << typeInt << std::endl;
+
+	if (typeChar == "impossible" && typeFloat == 0) {
+		std::cout << "float: impossible" << std::endl;
+		std::cout << "double: impossible" << std::endl;
 		return;
-	
-	typeInt = stoi(literal);
+	}
+
+	if (typeChar != "impossible" && typeFloat - static_cast<int>(typeFloat) == 0) {
+		std::cout << "float: " << typeFloat << ".0f" << std::endl;
+		std::cout << "double: " << typeDouble << ".0" << std::endl;
+		return;
+	}
+
+	std::cout << "float: " << typeFloat << "f" << std::endl;
+	std::cout << "double: " << typeDouble << std::endl;
 }
