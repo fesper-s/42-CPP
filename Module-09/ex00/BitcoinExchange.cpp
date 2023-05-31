576a304911d258bfb3f503d47940f3d75eea7a3c
fesper-s <fesper-s@student.42.rio>
2023-05-31 17:33:38 -0300

    updating2 CPP09/ex01

--- Module-09/ex00/BitcoinExchange.cpp
+++ Module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 17:39:23 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/05/31 13:32:44 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:32:37 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,40 @@ BitcoinExchange BitcoinExchange::operator=(const BitcoinExchange &rhs) {
 	return *this;
 }
 
-std::map<std::string, int> BitcoinExchange::getMap() const {
+std::map<std::string, float> BitcoinExchange::getMap() const {
 	return this->_map;
 }
 
-void BitcoinExchange::setMap(const std::map<std::string, int> &map) {
+void BitcoinExchange::setMap(const std::map<std::string, float> &map) {
 	this->_map = map;
 }
 
-void BitcoinExchange::exchange(const std::string &fileName) {
-	std::ifstream file(fileName);
-	if (!file.is_open())
-		throw std::runtime_error("Error: File doesn't exist");
+void BitcoinExchange::setDataBase() {
+	std::ifstream dataFile("data.csv");
+	if (!dataFile.is_open())
+		throw std::runtime_error("Error: 'data.csv' file is missing");
 	std::string line;
-	while (getline(file, line)) {
-		
+	getline(dataFile, line);
+	while (getline(dataFile, line)) {
+		std::string date = line.substr(0, 10);
+		std::string strValue = line.substr(11);
+		float value = atof(strValue.c_str());
+		this->_map.insert(std::make_pair(date, value));
 	}
-	file.close();
+	dataFile.close();	
+}
+
+static void validateInputFile(const std::string &fileName) {
+	std::ifstream inputFile(fileName);
+	if (!inputFile.is_open())
+		throw std::runtime_error("Error: Input file doesn't exist");
+	
+	inputFile.close();
+}
+
+void BitcoinExchange::exchange(const std::string &fileName) {
+	setDataBase();
+	for (std::map<std::string, float>::iterator it = this->_map.begin(); it != this->_map.end(); it++)
+		std::cout << it->first << " and " << it->second << std::endl;
+	validateInputFile(fileName);
 }
