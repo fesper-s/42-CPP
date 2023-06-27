5c64b95a5830aea039a45ce55bad1fc0a7116568
fesper-s <fesper-s@student.42.rio>
2023-06-26 23:11:49 -0300

    updating 09/ex02

--- Module-09/ex02/PmergeMe.cpp
+++ Module-09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 15:02:10 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/06/16 16:37:35 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/06/20 18:22:16 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,21 @@ bool PmergeMe::validateArg(char **nbrs) {
 				return false;
 		}
 		int buffer = atoi(nbrs[i]);
-		this->_vector.push_back(buffer);
+		this->_list.push_back(buffer);
 	}
 	return true;
+}
+
+void PmergeMe::mergeSort() {
+	size_t listSize = this->_list.size();
+	if (listSize <= 1)
+		return;
+	int i = 0;
+	for (std::list<int>::iterator lit; lit != this->_list.end(); ++lit) {
+		if (i < this->_list.size() / 2)
+			
+		i++;
+	}
 }
 
 void PmergeMe::execute(char **nbrs) {
@@ -43,4 +55,11 @@ void PmergeMe::execute(char **nbrs) {
 		std::cerr << "Error" << std::endl;
 		return;
 	}
+
+	std::cout << "---vector content---" << std::endl;
+	for (std::vector<int>::iterator vit = this->_vector.begin(); vit != this->_vector.end(); ++vit)
+		std::cout << *vit << std::endl;
+	std::cout << std::endl << "---list content---" << std::endl;
+	for (std::list<int>::iterator lit = this->_list.begin(); lit != this->_list.end(); ++lit)
+		std::cout << *lit << std::endl;
 }
--- Module-09/ex02/PmergeMe.hpp
+++ Module-09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 14:55:58 by fesper-s          #+#    #+#             */
-/*   Updated: 2023/06/16 16:24:46 by fesper-s         ###   ########.fr       */
+/*   Updated: 2023/06/20 18:38:03 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <iostream>
 # include <list>
 # include <vector>
+# include <array>
 
 class PmergeMe {
 	private:
@@ -29,6 +30,8 @@ class PmergeMe {
 		PmergeMe operator=(const PmergeMe &rhs);
 
 		bool validateArg(char **nbrs);
+		void mergeSort();
+		void merge();
 		void execute(char **nbrs);
 };
 
